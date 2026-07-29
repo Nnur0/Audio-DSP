3e06f020791e34ec767bcc235a9903ac92bf9476
Nuro Savelsberg <savelsbergnuro@gmail.com>
2026-07-29 20:17:13 +0200

    configure for esp32

--- main/filter.c
+++ main/filter.c
@@ -164,54 +164,55 @@ void filter(void *args)
     size_t r_bytes = 0;
     size_t w_bytes = 0;
 
-    ESP_ERROR_CHECK(i2s_channel_enable(rx_chan));
+    //ESP_ERROR_CHECK(i2s_channel_enable(rx_chan));
     ESP_ERROR_CHECK(i2s_channel_enable(tx_chan_1));
     ESP_ERROR_CHECK(i2s_channel_enable(tx_chan_2));    
 
     while (true) {
-        if (i2s_channel_read(rx_chan, in_buf, EXAMPLE_BUFF_SIZE, &r_bytes, 1000) == ESP_OK) {
-            // uint32_t start = esp_cpu_get_cycle_count();
-            // if (r_bytes != EXAMPLE_BUFF_SIZE) printf("Bytes: %d\n", r_bytes);
-            size_t frames = r_bytes / (2 * sizeof(int32_t));
-            process_block(in_buf, M_Buf, H_L_Buf, frames);
+        // if (i2s_channel_read(rx_chan, in_buf, EXAMPLE_BUFF_SIZE, &r_bytes, 1000) == ESP_OK) {
+        //     // uint32_t start = esp_cpu_get_cycle_count();
+        //     // if (r_bytes != EXAMPLE_BUFF_SIZE) printf("Bytes: %d\n", r_bytes);
+        //     size_t frames = r_bytes / (2 * sizeof(int32_t));
+        //     process_block(in_buf, M_Buf, H_L_Buf, frames);
 
-            i2s_channel_write(tx_chan_1, M_Buf, r_bytes, &w_bytes, portMAX_DELAY);
-            i2s_channel_write(tx_chan_2, H_L_Buf, r_bytes, &w_bytes, portMAX_DELAY);
+        //     i2s_channel_write(tx_chan_1, M_Buf, r_bytes, &w_bytes, portMAX_DELAY);
+        //     i2s_channel_write(tx_chan_2, H_L_Buf, r_bytes, &w_bytes, portMAX_DELAY);
             
             
-            // pushNewX(in_buf);
-            // calculateY(M_Buf, H_L_Buf);
-            // mapOutput(M_Buf, H_L_Buf);
+        //     // pushNewX(in_buf);
+        //     // calculateY(M_Buf, H_L_Buf);
+        //     // mapOutput(M_Buf, H_L_Buf);
 
-            // clocks[it] = esp_cpu_get_cycle_count() - start;
-            // it++;
-            // if (it == 1023)
-            // {
-            //     for (size_t i = 0; i < 1024; i++)
-            //     {
-            //         sum += clocks[i];
-            //     }
-            //     printf("Avg calc cycles: %llu\n", sum >> 10);
-            //     //printf("Avg calc cycles: %lu\n", clocks[4096]);
-            //     sum = 0;
-            //     it = 0;
-            // }
+        //     // clocks[it] = esp_cpu_get_cycle_count() - start;
+        //     // it++;
+        //     // if (it == 1023)
+        //     // {
+        //     //     for (size_t i = 0; i < 1024; i++)
+        //     //     {
+        //     //         sum += clocks[i];
+        //     //     }
+        //     //     printf("Avg calc cycles: %llu\n", sum >> 10);
+        //     //     //printf("Avg calc cycles: %lu\n", clocks[4096]);
+        //     //     sum = 0;
+        //     //     it = 0;
+        //     // }
 
-            //i2s_channel_write(tx_chan_1, in_buf, EXAMPLE_BUFF_SIZE, &w_bytes, 10); // <---- M_buf
-            //i2s_channel_write(tx_chan_2, in_buf, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
+        //     //i2s_channel_write(tx_chan_1, in_buf, EXAMPLE_BUFF_SIZE, &w_bytes, 10); // <---- M_buf
+        //     //i2s_channel_write(tx_chan_2, in_buf, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
 
-            // in_buf[1] = in_buf[1] * -1;
-            // in_buf[3] = in_buf[3] * -1;
+        //     // in_buf[1] = in_buf[1] * -1;
+        //     // in_buf[3] = in_buf[3] * -1;
             
-            // memcpy(w_buf1, in_buf, EXAMPLE_BUFF_SIZE);
-            // memcpy(w_buf2, in_buf, EXAMPLE_BUFF_SIZE);
-            // i2s_channel_write(tx_chan_1, w_buf1, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
-            // i2s_channel_write(tx_chan_2, w_buf2, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
-        } 
-        else {
-            printf("Read Task: i2s read failed\n");
-        }
-        //vTaskDelay(pdMS_TO_TICKS(200));
+        //     // memcpy(w_buf1, in_buf, EXAMPLE_BUFF_SIZE);
+        //     // memcpy(w_buf2, in_buf, EXAMPLE_BUFF_SIZE);
+        //     // i2s_channel_write(tx_chan_1, w_buf1, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
+        //     // i2s_channel_write(tx_chan_2, w_buf2, EXAMPLE_BUFF_SIZE, &w_bytes, 1000);
+        // } 
+        // else {
+        //     printf("Read Task: i2s read failed\n");
+        // }
+        printf("test\n");
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
     free(in_buf);
     free(M_Buf);
--- main/i2s.c
+++ main/i2s.c
@@ -1,8 +1,7 @@
 #include "i2s.h"
-
+#include "esp_log.h"
 i2s_chan_handle_t                tx_chan_1;
 i2s_chan_handle_t                tx_chan_2;       
-i2s_chan_handle_t                rx_chan;
 
 void init_i2s(void)
 {
@@ -11,28 +10,24 @@ void init_i2s(void)
         .dma_desc_num = 6,
         .dma_frame_num = 240,
         .id = I2S_NUM_0,
-        .role = I2S_ROLE_SLAVE,
+        .role = I2S_ROLE_MASTER,
         .intr_priority = 0,
         .auto_clear = true,
     };
-    ESP_ERROR_CHECK(i2s_new_channel(&d_chan_cfg, &tx_chan_1, &rx_chan));
+    ESP_ERROR_CHECK(i2s_new_channel(&d_chan_cfg, &tx_chan_1, NULL));
 
     i2s_chan_config_t s_chan_cfg = {
         .dma_desc_num = 6,
         .dma_frame_num = 240,
         .id = I2S_NUM_1,
-        .role = I2S_ROLE_SLAVE,
+        .role = I2S_ROLE_MASTER,
         .intr_priority = 0,
         .auto_clear = true,
     };
     ESP_ERROR_CHECK(i2s_new_channel(&s_chan_cfg, &tx_chan_2, NULL));
 
     i2s_std_config_t d_cfg = {
-        .clk_cfg  = {
-            .sample_rate_hz = CLOCK,
-            .clk_src = I2S_CLK_SRC_PLL_160M,
-            .mclk_multiple = 256U,
-        },                          
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(CLOCK),        
         //.clk_cfg  =I2S_STD_CLK_DEFAULT_CONFIG(CLOCK),
         .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
@@ -40,7 +35,7 @@ void init_i2s(void)
             .bclk = BCLK,
             .ws   = WS,
             .dout = DOUT_1,
-            .din  = DIN,
+            .din  = I2S_GPIO_UNUSED,
             .invert_flags = {
                 .mclk_inv = false,
                 .bclk_inv = false,
@@ -50,11 +45,7 @@ void init_i2s(void)
     };
 
     i2s_std_config_t s_cfg = {
-        .clk_cfg  = {
-            .sample_rate_hz = CLOCK,
-            .clk_src = I2S_CLK_SRC_PLL_160M,
-            .mclk_multiple = 256U,
-        },                          
+        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG(CLOCK),  
         //.clk_cfg  =I2S_STD_CLK_DEFAULT_CONFIG(CLOCK),
         .slot_cfg = I2S_STD_PHILIPS_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO),
         .gpio_cfg = {
@@ -71,7 +62,15 @@ void init_i2s(void)
         },
     };
     /* Initialize the channels */
-    ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_chan, &d_cfg));
+    // ESP_ERROR_CHECK(i2s_channel_init_std_mode(rx_chan, &d_cfg));
+    // ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_chan_1, &d_cfg));
+    // ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_chan_2, &s_cfg));
+
+    ESP_LOGI("TEST", "2");
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_chan_1, &d_cfg));
+
+    ESP_LOGI("TEST", "3");
     ESP_ERROR_CHECK(i2s_channel_init_std_mode(tx_chan_2, &s_cfg));
+
+    ESP_LOGI("TEST", "4");
 }
